4d4a07ddc891ee88397a65ce624d2f6a96a4373c
SiamKing <thesiameseelectric@gmail.com>
2017-08-21 19:48:50 -0700

    the children of React

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,17 @@
-// Code Invitation Component Here
+import React from 'react';
+
+export default class Invitation extends React.Component {
+  render() {
+    const invitationChildren = React.Children.map(this.props.children, child => {
+      return (
+        <p>{child}</p>
+      )
+    });
+    return (
+      <div className="invitation">
+        <h1>You've been invited!</h1>
+        {invitationChildren}
+      </div>
+    );
+  }
+}
